7bbfc927f22e05abaab49eacdad3b2c85c608667
shreejalmaharjan-27 <shreejal@rafled.com>
2023-01-05 08:59:39 +0545

    add comments

--- src/main.cpp
+++ src/main.cpp
@@ -20,14 +20,23 @@ Adafruit_SH1106 display(OLED_RESET);
 
 void setup(void)
 {
+
+  // begin serial console
   Serial.begin(9600);
+
+  // initialize display
   display.begin(SH1106_SWITCHCAPVCC, 0x3C);
+
+  // display Adafruit splash screen
   display.display();
   delay(2000);
+
   display.clearDisplay();
   display.setTextSize(3);
 
   Serial.println("Arduino Digital Temperature // Serial Monitor Version"); //Print a message
+
+  // start sensors
   sensors.begin();
 
 }
@@ -37,29 +46,35 @@ void loop(void)
   sensors.requestTemperatures();  
 
   float temp = sensors.getTempCByIndex(0);
+
+  // This is the ASCII code for Degree symbol `°` 
   char deg = 248;
 
   String str;
   String console;
 
-  str+= String(temp);
-  str+= deg;
-  str+="C";
+  // string for displaying on the OLED Screen
+  str += String(temp);
+  str += deg;
+  str +="C";
 
+  // string for displaying on the Serial console
   console += "Temperature is ";
   console += String(temp);
   console += " celcius";
 
-
-
+  // print to console
   Serial.println(console);
 
   display.clearDisplay();
-  display.setCursor(0,0);
+  display.setCursor(0,0); 
   display.setTextColor(WHITE);
   display.setTextSize(3);
-  display.println(str);
+  display.println(str); // set string to display on oled screen
 
+  // display the string
   display.display();
+
+  // update data every 1 second
   delay(1000);
 }
